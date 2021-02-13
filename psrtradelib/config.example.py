61e4df5248d49a716657c989d1481363e61c9820
philliprearick <psrearick@gmail.com>
2021-02-13 04:23:41 -0500

    remove config.py from git, add example config file

---
+++ psrtradelib/config.example.py
@@ -0,0 +1,4 @@
+class config():
+    def __init__(self):
+        self.ameritrade_api_key = ""
+        self.alpha_vantage_api_key = ""
--- psrtradelib/config.py
+++
@@ -1,3 +0,0 @@
-class config():
-    def __init__(self):
-        self.ameritrade_api_key = "AAHDLK9MEPGU1UJ0NMNZ3AQXPTVO1RV4"
